65d07246f0617022cf7032e7f3b8f8d661dc083a
cristiprg <cristian.prigoana@yahoo.com>
2017-03-18 23:15:28 +0100

    Make the test configurable and use sockets for input

--- bdapro-ws1617-spark-jobs/src/main/scala/de/tu_berlin/dima/bdapro/spark/mlalg/StreamingKMeansSparkVersion.scala
+++ bdapro-ws1617-spark-jobs/src/main/scala/de/tu_berlin/dima/bdapro/spark/mlalg/StreamingKMeansSparkVersion.scala
@@ -4,42 +4,58 @@ import de.tu_berlin.dima.bdapro.spark.mlalg.util.CSVFileSource
 import org.apache.spark.SparkConf
 import org.apache.spark.mllib.clustering.{StreamingKMeans, StreamingKMeansGlobalState}
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.{Milliseconds, Seconds, StreamingContext}
 
 /**
   * Created by cristiprg on 8-1-17.
   */
 object StreamingKMeansSparkVersion {
 
-  val batchIntervalSeconds = 1
+  var batchIntervalMilliseconds: Long = 1
   val monitoringIntervalSeconds = 1
-  var filePath = ""
+  var redisServerAddress = ""
   val numberOfClusters = 4
+  var textSocketStreamAddress: String = ""
 
-  // Global variable across threads
+  var whichKMeans: String = ""
+
+  var modelConfigGlobalState: StreamingKMeansGlobalState = null
   var modelConfig: StreamingKMeans = null
 
   def creatingFunc(): StreamingContext = {
 
     val conf = new SparkConf()
       .setAppName("StreamingKMeansSparkVersion")
-      .setMaster("local[*]")
+        //.setMaster("local[*]")
 
-    val ssc = new StreamingContext(conf, Seconds(batchIntervalSeconds))
-    val initialState: Array[Vector] = Array(Vectors.dense(0.0, 0.0, 0.0, 0.0), Vectors.dense(1.0, 1.0, 1.0, 1.0),
-      Vectors.dense(2.0, 2.0, 2.0, 2.0), Vectors.dense(3.0, 3.0, 3.0, 3.0))
+    val ssc = new StreamingContext(conf, Milliseconds(batchIntervalMilliseconds))
+    val initialState: Array[Vector] = Array(Vectors.dense(4,5,3,5), Vectors.dense(5,5,5,4),
+      Vectors.dense(5,5,4,5), Vectors.dense(5,4,5,5))
 
-    val trainingDataStream = ssc.receiverStream(new CSVFileSource(100, "/home/cristiprg/Spark/iris6.csv" )).map(Vectors.parse)
+    //val trainingDataStream = ssc.receiverStream(new CSVFileSource(100, "/home/cristiprg/Spark/iris6.csv" )).map(Vectors.parse)
     //val trainingDataStream = ssc.textFileStream(filePath).flatMap(_.split("\n")).map('[' + _ + ']').map(Vectors.parse)
+    val trainingDataStream = ssc.socketTextStream(textSocketStreamAddress, 9999).map('[' + _ + ']').map(Vectors.parse)
 
     //trainingDataStream.print()
-    modelConfig = new StreamingKMeans()
-      .setK(numberOfClusters)
-      .setDecayFactor(0.9)
-      .setInitialCenters(initialState, Array(1, 1, 1, 1))
-      //.setRandomCenters(4, 1)
+    whichKMeans match {
+      case "ORIGINAL" =>
+        modelConfig = new StreamingKMeans()
+          .setK(numberOfClusters)
+          .setDecayFactor(0.9)
+          .setInitialCenters(initialState, Array(1, 1, 1, 1))
+        //.setRandomCenters(4, 1)
+        modelConfig.trainOn(trainingDataStream)
 
-    modelConfig.trainOn(trainingDataStream)
+      case "GLOBALSTATE" =>
+        modelConfigGlobalState = new StreamingKMeansGlobalState(redisServerAddress)
+        .setK(numberOfClusters)
+        .setDecayFactor(0.9)
+        .setInitialCenters(initialState, Array(1, 1, 1, 1))
+        //.setRandomCenters(4, 1)
+
+        modelConfigGlobalState.trainOn(trainingDataStream)
+    }
+
 
 
     //modelConfig.latestModel().clusterCenters.foreach(println)
@@ -50,20 +66,23 @@ object StreamingKMeansSparkVersion {
 
   def main(args: Array[String]): Unit = {
 
-    if (args.length != 1) {
-      println("Usage: <jar> inputPath")
+    if (args.length != 4) {
+      println("Usage: <jar> redisServerAddress batchIntervalMilliseconds textSocketStreamAddress <ORIGINAL|GLOBALSTATE>")
       System.exit(-1)
     }
 
-    filePath = args(0)
+    redisServerAddress = args(0)
+    batchIntervalMilliseconds = args(1).toLong
+    textSocketStreamAddress = args(2)
+    whichKMeans = args(3)
 
     // 1. Set up the monitoring thread
     new Thread("Cluster Centers Thread") {
       override def run(): Unit = {
         while(true) {
-          if (modelConfig != null)
-            modelConfig.latestModel().clusterCenters.foreach(print)
-            println
+          if (modelConfigGlobalState != null) {modelConfigGlobalState.latestModel().clusterCenters.foreach(print);println}
+          if (modelConfig != null) {modelConfig.latestModel().clusterCenters.foreach(print);println}
+
           Thread.sleep(monitoringIntervalSeconds * 1000L)
         }
       }
